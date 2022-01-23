f6f4ecb851e7535c5e1f238a53247e0242dd8662
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-01-23 13:22:33 +0530

    created AddContact.js component and added that

--- src/components/subcomponents/AddContact.js
+++ src/components/subcomponents/AddContact.js
@@ -1,7 +1,24 @@
 import React from 'react';
 
 const AddContact = () => {
-  return <div></div>;
+  return (
+    <div className='ui main'>
+      <h2>Add Contact</h2>
+      <form className='ui form'>
+        <div className='field'>
+          <label>Name</label>
+          <input type='text' name='name' id='name' placeholder='Name' />
+        </div>
+
+        <div className='field'>
+          <label>Email</label>
+          <input type='text' name='email' id='email' placeholder='Email' />
+        </div>
+
+        <button className='ui button blue'>Add</button>
+      </form>
+    </div>
+  );
 };
 
 export default AddContact;
