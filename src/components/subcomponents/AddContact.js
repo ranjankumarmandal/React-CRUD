91f2def5c41f71b976d230869f342512278a59ac
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-01-23 16:33:32 +0530

    added post functionality

--- src/components/subcomponents/AddContact.js
+++ src/components/subcomponents/AddContact.js
@@ -1,58 +1,112 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { v4 } from 'uuid';
 import axios from 'axios';
 
 const AddContact = () => {
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
+  const [empty, setEmpty] = useState(true);
+
+  useEffect(() => {
+    const timeId = setTimeout(() => {
+      // After 3 seconds set the show value to false
+      setEmpty(false);
+    }, 3000);
+
+    return () => {
+      clearTimeout(timeId);
+    };
+  }, [empty]);
 
   const handleSubmission = async (e) => {
     e.preventDefault();
 
-    try {
-      let response = await axios.post(
-        'https://ranjan-json-server.herokuapp.com/posts',
-        { v4, title, content }
-      );
-      console.log(response.data);
-      setTitle('');
-      setContent('');
-      window.location.reload();
-    } catch (error) {}
+    if (title == '' || content == '') {
+      setEmpty(true);
+    } else {
+      try {
+        let response = await axios.post(
+          'https://ranjan-json-server.herokuapp.com/posts',
+          { v4, title, content }
+        );
+        console.log(response.data);
+        setTitle('');
+        setContent('');
+        window.location.reload();
+      } catch (error) {}
+    }
   };
 
   return (
     <div className='ui main'>
-      <h2>Add Contact</h2>
-      <form className='ui form'>
-        <div className='field'>
-          <label>Name</label>
-          <input
-            type='text'
-            name='name'
-            id='name'
-            placeholder='Name'
-            onChange={(e) => setTitle(e.target.value)}
-            value={title}
-          />
-        </div>
+      {empty ? (
+        <div>
+          <div className='ui red message'>Please Enter Name and Email both</div>
+          <h2>Add Contact</h2>
+          <form className='ui form'>
+            <div className='field'>
+              <label>Name</label>
+              <input
+                type='text'
+                name='name'
+                id='name'
+                placeholder='Name'
+                onChange={(e) => setTitle(e.target.value)}
+                value={title}
+              />
+            </div>
 
-        <div className='field'>
-          <label>Email</label>
-          <input
-            type='text'
-            name='email'
-            id='email'
-            placeholder='Email'
-            onChange={(e) => setContent(e.target.value)}
-            value={content}
-          />
-        </div>
+            <div className='field'>
+              <label>Email</label>
+              <input
+                type='text'
+                name='email'
+                id='email'
+                placeholder='Email'
+                onChange={(e) => setContent(e.target.value)}
+                value={content}
+              />
+            </div>
 
-        <button className='ui button blue' onClick={handleSubmission}>
-          Add
-        </button>
-      </form>
+            <button className='ui button blue' onClick={handleSubmission}>
+              Add
+            </button>
+          </form>
+        </div>
+      ) : (
+        <div>
+          <h2>Add Contact</h2>
+          <form className='ui form'>
+            <div className='field'>
+              <label>Name</label>
+              <input
+                type='text'
+                name='name'
+                id='name'
+                placeholder='Name'
+                onChange={(e) => setTitle(e.target.value)}
+                value={title}
+              />
+            </div>
+
+            <div className='field'>
+              <label>Email</label>
+              <input
+                type='text'
+                name='email'
+                id='email'
+                placeholder='Email'
+                onChange={(e) => setContent(e.target.value)}
+                value={content}
+              />
+            </div>
+
+            <button className='ui button blue' onClick={handleSubmission}>
+              Add
+            </button>
+          </form>
+        </div>
+      )}
     </div>
   );
 };
