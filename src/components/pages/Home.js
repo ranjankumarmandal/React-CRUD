dbafd1e8ff5fb5899a1c88094af4f21e157d5236
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-01-23 13:15:35 +0530

    created Header.js component and added

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,10 +1,14 @@
 import React, { Fragment } from 'react';
 import Header from '../subcomponents/Header';
+import AddContact from '../subcomponents/AddContact';
+import ContactList from '../subcomponents/ContactList';
 
 const Home = () => {
   return (
     <Fragment>
       <Header />
+      <AddContact />
+      <ContactList />
     </Fragment>
   );
 };
--- src/components/subcomponents/Header.js
+++ src/components/subcomponents/Header.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <div>
-      <p>THis is header component</p>
+    <div className='ui fixed menu'>
+      <div className='ui container center'>
+        <h2>Contact Manager</h2>
+      </div>
     </div>
   );
 };
