1469d05562758aceeb01cb9ca4fa9368720bc738
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-01-23 15:36:15 +0530

    added logic for getting contact lists from json-server with axios

--- src/components/subcomponents/AddContact.js
+++ src/components/subcomponents/AddContact.js
@@ -1,21 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { v4 } from 'uuid';
+import axios from 'axios';
 
 const AddContact = () => {
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+
+  const handleSubmission = async (e) => {
+    e.preventDefault();
+
+    try {
+      let response = await axios.post(
+        'https://ranjan-json-server.herokuapp.com/posts',
+        { v4, title, content }
+      );
+      console.log(response.data);
+      setTitle('');
+      setContent('');
+      window.location.reload();
+    } catch (error) {}
+  };
+
   return (
     <div className='ui main'>
       <h2>Add Contact</h2>
       <form className='ui form'>
         <div className='field'>
           <label>Name</label>
-          <input type='text' name='name' id='name' placeholder='Name' />
+          <input
+            type='text'
+            name='name'
+            id='name'
+            placeholder='Name'
+            onChange={(e) => setTitle(e.target.value)}
+            value={title}
+          />
         </div>
 
         <div className='field'>
           <label>Email</label>
-          <input type='text' name='email' id='email' placeholder='Email' />
+          <input
+            type='text'
+            name='email'
+            id='email'
+            placeholder='Email'
+            onChange={(e) => setContent(e.target.value)}
+            value={content}
+          />
         </div>
 
-        <button className='ui button blue'>Add</button>
+        <button className='ui button blue' onClick={handleSubmission}>
+          Add
+        </button>
       </form>
     </div>
   );
--- src/components/subcomponents/ContactList.js
+++ src/components/subcomponents/ContactList.js
@@ -1,24 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ContactCard from './ContactCard';
+import axios from 'axios';
 
 const ContactList = () => {
-  let contacts = [
-    {
-      id: '1',
-      title: 'ranjan',
-      content: 'ranjan45@gmail.com',
-    },
-    {
-      id: '2',
-      title: 'brad',
-      content: 'brad@gmail.com',
-    },
-  ];
+  const [contacts, setContacts] = useState([]);
+
+  useEffect(async () => {
+    try {
+      let response = await axios.get(
+        'https://ranjan-json-server.herokuapp.com/posts'
+      );
+      setContacts(response.data);
+      console.log('Content Loaded');
+    } catch (error) {
+      console.log('Something went wrong');
+    }
+  }, []);
 
   return (
     <div className='ui celled list'>
       {contacts.map((contact) => (
-        <ContactCard contact={contact} />
+        <ContactCard key={contact.id} contact={contact} />
       ))}
     </div>
   );
