e084ee237960fe64b12cd9b43d7271cc776dbba4
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-01-23 14:51:47 +0530

    all the components jsx completed

--- src/components/subcomponents/ContactCard.js
+++ src/components/subcomponents/ContactCard.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import user from '../../images/user.png';
 
 const ContactCard = ({ contact }) => {
   return (
     <div className='item'>
+      <img src={user} alt='user' className='ui avatar image' />
       <div className='content'>
         <div className='header'>{contact.title}</div>
         <div>{contact.content}</div>
       </div>
-      <i className='trash alternate outline icon' style={{ color: 'red' }}></i>
+      <i
+        className='trash alternate outline icon'
+        style={{ color: 'red', marginTop: '7px' }}
+      ></i>
     </div>
   );
 };
