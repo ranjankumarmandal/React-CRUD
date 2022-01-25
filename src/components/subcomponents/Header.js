73a4e65f30aacd2d4ade8a72d633deac15eeeb42
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-01-25 12:32:15 +0530

    completed all the crud functionality and added About functionality

--- src/components/subcomponents/Header.js
+++ src/components/subcomponents/Header.js
@@ -1,10 +1,22 @@
 import React from 'react';
 
 const Header = () => {
+  const about = (e) => {
+    alert('This application is made by Ranjan Kumar Mandal');
+  };
+
   return (
     <div className='ui fixed menu'>
       <div className='ui container center'>
         <h2>Contact Manager</h2>
+      </div>
+      <div className='last' onClick={about}>
+        <a
+          href='https://www.linkedin.com/in/ranjan-kumar-m-818367158/'
+          target='_blank'
+        >
+          About
+        </a>
       </div>
     </div>
   );
